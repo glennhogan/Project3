dcc170ba1c308e7eda3a64bdbfa97b656266c542
Justin Brady <jmb4zz@virginia.edu>
2023-11-10 13:11:46 -0500

    adding import statements

---
+++ SRC/model.py
@@ -0,0 +1,12 @@
+import os
+import warnings
+warnings.filterwarnings('ignore')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from statsmodels.tsa.statools import adfuller
+from statsmodels.tsa.seasonal import seasonal_decompose
+from statsmodels.tsa.arima_model import ARIMA
+from pmdarima.arima import auto_arima
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+import math
